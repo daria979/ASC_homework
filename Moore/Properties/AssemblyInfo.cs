b228510fbc9eaf305b8a454474544e64019f9e11
Trip Daria <daria.trip001@gmail.com>
2020-10-18 20:35:14 +0300

    Project

--- Moore/Properties/AssemblyInfo.cs
+++ Moore/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1dd099b7-647b-4d3b-af92-55e743c5adb1")]
+[assembly: Guid("ef39635b-1d0b-4fde-93d8-1f9f8015180f")]
 
 // Version information for an assembly consists of the following four values:
 //
